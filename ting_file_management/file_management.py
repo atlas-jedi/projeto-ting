5056282ed8a5c3077a20a55be484c7b33e2367c3
André Pestana Silva <atlas.ifpa@gmail.com>
2023-06-27 12:29:50 -0300

    feat: implementa função file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+            return None
+
+        with open(path_file, "r", encoding="utf-8") as file:
+            file_data = file.readlines()
+            return [line.strip() for line in file_data]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
