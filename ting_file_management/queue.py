ef0150a0927242df3ed11b974c27a6f6e41edc3d
André Pestana Silva <atlas.ifpa@gmail.com>
2023-06-27 02:33:46 -0300

    feat: cria classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,26 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
+
+    def __str__(self):
+        return self.data
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
+        self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        item_removed = self.data[0]
+        self.data.pop(0)
+        self.__length -= 1
+        return item_removed
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not isinstance(index, int) or (self.__length <= index) or index < 0:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.data[index]
