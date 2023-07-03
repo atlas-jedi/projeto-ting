e8ddf0a73a3a2a581bf1877cb637cebbabd6977f
André Pestana Silva <atlas.ifpa@gmail.com>
2023-07-03 20:36:51 -0300

    feat: implementa função exists_word em word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.abstract_queue import AbstractQueue
 
 
-def search_by_word(word, instance):
+def exists_word(word: str, instance: AbstractQueue):
+    word = word.lower()
+    results = []
+
+    for file in instance.data:
+        file_name = file["nome_do_arquivo"]
+        file_lines = file["linhas_do_arquivo"]
+
+        ocurrences = [
+            {"linha": j + 1}
+            for j, line in enumerate(file_lines)
+            if word in line.lower()
+        ]
+
+        if ocurrences:
+            results.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": ocurrences,
+                }
+            )
+
+    return results
+
+
+def search_by_word(word: str, instance: AbstractQueue):
     """Aqui irá sua implementação"""
